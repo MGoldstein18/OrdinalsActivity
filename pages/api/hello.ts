3146800fce1d984134b39c35182cc00ad71d3d26
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2024-05-09 17:10:57 +0200

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string;
-};
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: "John Doe" });
+  console.log('req', req.body);
+
+  const { address } = JSON.parse(req.body);
+
+  if (!address) {
+    res.status(400).json({ error: 'Address not found' });
+  }
+
+  const token = process.env.TOKEN;
+
+  if (!token) {
+    res.status(400).json({ error: 'Token not found' });
+  }
+
+  const response = await fetch(
+    `https://api.ordiscan.com/v1/address/${address}/activity`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
 }
